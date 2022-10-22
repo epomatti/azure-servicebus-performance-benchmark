fb2d996b3f7924c72c2bfe3940eb426feaaa0d64
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-10-22 17:34:37 -0300

    sender

--- app/src/main/java/io/pomatti/azure/servicebus/benchmark/MessageMachine.java
+++ app/src/main/java/io/pomatti/azure/servicebus/benchmark/MessageMachine.java
@@ -3,6 +3,8 @@ package io.pomatti.azure.servicebus.benchmark;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,18 +19,29 @@ public class MessageMachine {
     this.sender = sender;
   }
 
-  public void start() {
+  public void start() throws RuntimeException {
     List<String> dataset = getLargeDataset();
-    dataset.parallelStream().forEach(body -> {
+
+    int size = Integer.parseInt(Config.getProperty("app.sender_threads"));
+    ForkJoinPool pool = new ForkJoinPool(size);
+
+    pool.submit(() -> dataset.stream().parallel().forEach(body -> {
       sender.send(body);
-    });
-    logger.info("Finished stream");
+    }));
+
+    try {
+      pool.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+      pool.shutdown();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   private List<String> getLargeDataset() {
     logger.info("Started building datasource");
+    Integer qty = Integer.parseInt(Config.getProperty("app.message_quantity"));
     List<String> dataset = new ArrayList<>();
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < qty; i++) {
       dataset.add(UUID.randomUUID().toString());
     }
     logger.info(dataset.size() + "");
